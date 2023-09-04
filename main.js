5c908b5b80e2d60454037cba95664e1491b54e9c
Honore Solomon <honoresolomon@hotmail.com>
2023-09-04 12:43:57 -0700

    changed into factory and modules

--- main.js
+++ main.js
@@ -1,23 +1,87 @@
-// const Gameboard = (() => {
-//   let gBoard = ["","","","","","","","",""] ;
+const GameBoardModule = () => {
+  const gameBoard = document.querySelector('#gameBoard');
+  const startCells = [
+    "","","","","","","","",""
+  ]
+  
+  function createBoard(addSymbol) {
 
-//   const render = () => {
-//     let boardHTML = "";
-//     gBoard.forEach((square, index) => {
-//       boardHTML+= `<div class="square" id="square-${index}">${square}</div>`
-//     });
-//     document.querySelector("#gameboard").innerHTML = boardHTMfL;
+      startCells.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('square');
+        cellElement.id = index;
+        cellElement.addEventListener('click', addSymbol)
     
+        gameBoard.append(cellElement);
+      }
+        
+      )
+    }
 
-//   }
-//   return {render,}
-// }
-// )()
+  return {
+    createBoard
+  }
+}
 
-// const createPlayer = (name, mark) => {
+
+
+
+const GameLogicModule = () => {
+  const winningCombos = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+  ];
+  const infoDisplay = document.querySelector('#message');
+  const result = document.querySelector('#result-display');
+  let symbol = 'circle'
+  function addSymbol(e) {
+    
+    const symbolDisplay = document.createElement('div');
+    symbolDisplay.classList.add(symbol);
+    e.target.append(symbolDisplay)
+    symbol = symbol === 'circle' ? 'cross' : 'circle'
+    e.target.removeEventListener("click", addSymbol)
+    infoDisplay.textContent = `It is the ${symbol} 's turn`
+    checkScore()
+  }
+
+  function checkScore ()  {
+    let allSquares = Array.from(document.querySelectorAll('.square'));
+    function checkWinner(symbolToCheck) {
+      for (const combo of winningCombos) {
+        const wins = combo.every(cell =>
+          allSquares[cell].firstChild?.classList.contains(symbolToCheck))
+    
+          if (wins) {
+            document.querySelector('body').classList.add('blur')
+            result.textContent = `${symbolToCheck.charAt(0).toUpperCase() + symbolToCheck.slice(1)} Wins!!`
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            infoDisplay.textContent = ''
+            return
+          }
+      }
+    }
+    checkWinner('cross');
+    checkWinner('circle')
+  }
+  return {
+    addSymbol,
+    checkScore
+  }
+}
+
+const start = (() => {
+  const gameBoard = GameBoardModule();
+  const gameLogic = GameLogicModule()
+
+  gameBoard.createBoard(gameLogic.addSymbol)
+}
+)()
+// const createPlayer = (name, symbol) => {
 //   return {
 //     name, 
-//     mark
+//     symbol
 //   }
 // }
 
@@ -30,82 +94,11 @@
 //       players = [createPlayer(document.querySelector('#player1').value, 'X'),
 //       createPlayer(document.querySelector('#player2').value,'O')]
 //     }
+//     renderBoard()
 // })()
 
-// const startButton = document.querySelector('#start-button');
-// startButton.addEventListener('click', () => Gameboard.render )
-
-
-// MVP Build
-
-const gameBoard = document.querySelector('#gameBoard');
-const infoDisplay = document.querySelector('#message');
-const result = document.querySelector('#result-display')
-const startCells = [
-  "","","","","","","","",""
-]
-
-const winningCombos = [
-  [0,1,2],[3,4,5],[6,7,8],
-  [0,3,6],[1,4,7],[2,5,8],
-  [0,4,8],[2,4,6]
-]
-
-let symbol = 'circle'
-
-function createBoard() {
-
-  startCells.forEach((cell, index) => {
-    const cellElement = document.createElement('div');
-    cellElement.classList.add('square');
-    cellElement.id = index;
-    cellElement.addEventListener('click', addSymbol)
-
-    gameBoard.append(cellElement);
-  }
-    
-  )
-}
 
 
 
-function addSymbol(e) {
-  
-  const symbolDisplay = document.createElement('div');
-  symbolDisplay.classList.add(symbol);
-  e.target.append(symbolDisplay)
-  symbol = symbol === 'circle' ? 'cross' : 'circle'
-  e.target.removeEventListener("click", addSymbol)
-  infoDisplay.textContent = `It is the ${symbol} 's turn`
-  checkScore()
-}
 
-function checkScore ()  {
-  let allSquares = Array.from(document.querySelectorAll('.square'));
-  winningCombos.forEach(array => {
-    const circleWins = array.every(cell =>
-      allSquares[cell].firstChild?.classList.contains('circle'))
-
-      if (circleWins) {
-        result.textContent = "Circle Wins!!"
-        allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
-        infoDisplay.textContent = ''
-        return
-      }
-  })
-
-  winningCombos.forEach(array => {
-    const crossWins = array.every(cell =>
-      allSquares[cell].firstChild?.classList.contains('cross'))
-
-      if (crossWins) {
-        result.textContent = "Cross Wins!!"
-        allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
-        infoDisplay.textContent = ''
-        return
-      }
-  })
-}
-
-createBoard();
 
